b30c2c414a5b07ddda9303ae70b73fdae31d85b6
Admin <prabhat.ai20@bmsce.ac.in>
2023-12-01 23:32:15 +0530

    deploy-aws

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! This is a simple node js project deployed on AWS EC2 instance');
 });
 
 const port = process.env.PORT || 3000;
